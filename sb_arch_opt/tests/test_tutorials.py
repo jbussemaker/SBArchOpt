700d276a2389e23628419b0d7cd59b159dd81f05
Jasper Bussemaker <jasper.bussemaker@dlr.de>
2024-06-14 10:40:21 +0200

    Speed up tutorial notebook test

--- sb_arch_opt/tests/test_tutorials.py
+++ sb_arch_opt/tests/test_tutorials.py
@@ -1,15 +1,30 @@
 import os
 import pytest
 import testbook
+from testbook.client import TestbookNotebookClient
 
 _docs_path = f'{os.path.dirname(__file__)}/../../docs'
 _t = 60*20
 
 
 @pytest.mark.skipif(int(os.getenv('RUN_SLOW_TESTS', 0)) != 1, reason='Set RUN_SLOW_TESTS=1 to run slow tests')
-@testbook.testbook(f'{_docs_path}/tutorial.ipynb', execute=True, timeout=_t)
-def test_tutorial(tb):
-    pass
+@testbook.testbook(f'{_docs_path}/tutorial.ipynb', execute=False, timeout=_t)
+def test_tutorial(tb: TestbookNotebookClient):
+    code_cells = []
+    for cell in tb.cells:
+        if cell.cell_type == 'code':
+            code_cells.append(cell)
+
+    # Set less infills to reduce testing time
+    sbo_example_cell = code_cells[1]
+    code = sbo_example_cell.source.split('\n')
+    for i, line in enumerate(code):
+        if line.startswith('n_infill'):
+            code[i] = 'n_infill = 2'
+            break
+    sbo_example_cell.source = '\n'.join(code)
+
+    tb.execute()
 
 
 @pytest.mark.skipif(int(os.getenv('RUN_SLOW_TESTS', 0)) != 1, reason='Set RUN_SLOW_TESTS=1 to run slow tests')
