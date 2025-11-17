732c5c3003f52a10997525b3cacba78c7df3ef56
Jasper Bussemaker <jasper.bussemaker@dlr.de>
2025-11-17 08:05:59 +0100

    Stop checking for trieste and tpe tests

--- sb_arch_opt/tests/algo/test_trieste.py
+++ sb_arch_opt/tests/algo/test_trieste.py
@@ -11,9 +11,9 @@ def check_dependency():
     return pytest.mark.skipif(not HAS_TRIESTE, reason='Trieste dependencies not installed')
 
 
-@pytest.mark.skipif(int(os.getenv('RUN_SLOW_TESTS', 0)) != 1, reason='Set RUN_SLOW_TESTS=1 to run slow tests')
-def test_slow_tests():
-    assert HAS_TRIESTE
+# @pytest.mark.skipif(int(os.getenv('RUN_SLOW_TESTS', 0)) != 1, reason='Set RUN_SLOW_TESTS=1 to run slow tests')
+# def test_slow_tests():
+#     assert HAS_TRIESTE
 
 
 @check_dependency()
